9fcb7f4f8a8cd696c0a25a493c57d3bebee6f873
Kmilia <camelia.guerraoui@gmail.com>
2020-11-16 17:38:43 +0100

    Ajout de la javadoc pour la partie UDP Client

--- Code-Socket/src/stream_udp/EchoClient.java
+++ Code-Socket/src/stream_udp/EchoClient.java
@@ -1,8 +1,13 @@
-/***
+/**
  * EchoClient
- * Example of a TCP client 
- * Date: 10/01/04
- * Authors:
+ * A client connects to a specific server and then chats with other people.
+ * This class :
+ * - connects the client to the server using the UDP Protocol.
+ * - launchs a parallel reception thread
+ * - emits every messages to the server.
+ * @since 16/11/2021
+ * @author Camille Peltier, Camélia Guerraoui
+ * @see ReceptionMessageClientThread
  */
 package stream_udp;
 
@@ -10,50 +15,54 @@ import java.io.*;
 import java.net.*;
 
 
-
 public class EchoClient {
 
  
-  /**
-  *  main method
-  *  accepts a connection, receives a message from client then sends an echo to the client
-  **/
+    /**
+    * Main Method :
+    *   - connects the client to the server using the UDP Protocol.
+    *   - launchs a parallel reception thread
+    *   - emits every messages to the server.
+    * @Exception
+    * @See ReceptionMessageClientThread
+    */
     public static void main(String[] args) throws IOException {
+        // Server's Parameters 
+        final int SERVER_PORT = 3500; 
+        final String SERVER_NAME = "Macbook-Cami-3";
+        final InetAddress SERVER_ADDRESS = InetAddress.getByName(SERVER_NAME);
+        
+        // Group's Parameters 
+        final String GROUP_NAME = "228.5.6.7";
+        final InetAddress GROUP_ADDRESS = InetAddress.getByName(GROUP_NAME);
+        
+        MulticastSocket clientSocket = new MulticastSocket(SERVER_PORT);
+        clientSocket.joinGroup(GROUP_ADDRESS);
+        
         BufferedReader stdIn = null;
         stdIn = new BufferedReader(new InputStreamReader(System.in));
         
-        int serverPort = 3500; 
-        String serverHost = "Macbook-Cami-3";
-        InetAddress serverAddress = InetAddress.getByName(serverHost);
-        
-        // Create a datagram socket
-        InetAddress groupAddress = InetAddress.getByName("228.5.6.7");
-        MulticastSocket clientSocket = new MulticastSocket(serverPort);
-        clientSocket.joinGroup(groupAddress);
-        
         System.out.println("What's your pseudo?");
-        String pseudo = stdIn.readLine();
-        String line = "";
+        final String PSEUDO = stdIn.readLine();
+        String emitted_message = "";
         
-        ReceptionMessageClientThread reception = new ReceptionMessageClientThread(clientSocket, pseudo, groupAddress);
+        // Launch reception thread
+        ReceptionMessageClientThread reception = new ReceptionMessageClientThread(clientSocket, PSEUDO, GROUP_ADDRESS);
         reception.start();
         
-        while(!line.equals(pseudo+" left.")){
+        while(!emitted_message.equals(PSEUDO+" left.")){
 
-            // Build a request
-            line = pseudo+" : ";
-            line += stdIn.readLine();
-            if (line.equals(pseudo+" : .")){
-                line = pseudo+" left.";
+            // Build a message to emit
+            emitted_message = PSEUDO + " : " + stdIn.readLine();
+            if (emitted_message.equals(PSEUDO+" : .")){
+                emitted_message = PSEUDO+" left.";
             }
             
             byte[] buf = new byte[1000];
-            buf = line.getBytes();
+            buf = emitted_message.getBytes();
             
-            // Create a datagram packet destined for the server
-            DatagramPacket packet = new DatagramPacket(buf, buf.length, serverAddress, serverPort);
-           
-            // Send datagram packet to server
+            // Send a datagram packet destined for the server
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, SERVER_ADDRESS, SERVER_PORT);
             clientSocket.send(packet);
         }
 
--- Code-Socket/src/stream_udp/ReceptionMessageClientThread.java
+++ Code-Socket/src/stream_udp/ReceptionMessageClientThread.java
@@ -1,8 +1,11 @@
 /***
- * ClientThread
- * Example of a TCP server
- * Date: 14/12/08
- * Authors:
+ * ReceptionMessageClientThread
+ * This class extends Thread Class.
+ * It receives every messages for the client using the UDP Protocol
+ * It is launched by the class EchoClient.
+ * @since 16/11/2021
+ * @author Camille Peltier, Camélia Guerraoui
+ * @see EchoClient
  */
 
 package stream_udp;
@@ -14,42 +17,48 @@ import java.util.LinkedList;
 public class ReceptionMessageClientThread extends Thread {
 	
 	private MulticastSocket clientSocket;
-    private String pseudo;
-    private InetAddress groupAddress;
+    private final String PSEUDO;
+    private final InetAddress GROUP_ADDRESS;
 	
-	ReceptionMessageClientThread(MulticastSocket socketInit, String pseudoInit, InetAddress groupAddressInit) {
-		this.clientSocket = socketInit;
-        this.pseudo = pseudoInit;
-        this.groupAddress = groupAddressInit;
+    /**
+     * Constructor
+     * @param initialSocket client's socket 
+     * @param INITIAL_PSEUDO client's pseudonym 
+     * @param INITIAL_GROUP_ADDRESS client's group IP address
+     */
+	ReceptionMessageClientThread(MulticastSocket initialSocket, final String INITIAL_PSEUDO, final InetAddress INITIAL_GROUP_ADDRESS) {
+		this.clientSocket = initialSocket;
+        this.PSEUDO = INITIAL_PSEUDO;
+        this.GROUP_ADDRESS = INITIAL_GROUP_ADDRESS;
 	}
 
  	/**
-  	* receives a request from client then sends an echo to the client
-  	* @param clientSocket the client socket
-  	**/
+    * Main Method :
+    *   - receives every messages for the client from his group
+    * @Exception
+    */
 	public void run() {
         try {
              
-            String received = "";
-    		while (!received.equals(pseudo+" left.")) {
-                byte[] buf_response = new byte[1000];
-            
-                // Build a datagram packet for response
-                DatagramPacket packet_response = new DatagramPacket(buf_response, buf_response.length);
+            String received_message = "";
+    		while (!received_message.equals(this.PSEUDO+" left.")) {
+                
+                byte[] buf = new byte[1000];
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
                 
                 // Receive response
-                clientSocket.receive(packet_response);
-                received = new String(packet_response.getData());
-                System.out.println(received);
+                clientSocket.receive(packet);
+                received_message = new String(packet.getData());
+                System.out.println(received_message);
 
     		}
             System.out.println("FINI RECEPTION");
-            clientSocket.leaveGroup(groupAddress);
+            clientSocket.leaveGroup(this.GROUP_ADDRESS);
             clientSocket.close();
 
     	} catch (Exception e) {
         	System.err.println("Error in ReceptionClientThread:" + e); 
         }
-       }
+    }
   
   }
